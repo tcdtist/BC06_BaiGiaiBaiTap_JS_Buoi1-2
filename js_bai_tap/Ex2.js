160b24a33a918b5fae0d4f5680859e3315f18914
chithanh <chithanhdang1311@gmail.com>
2021-01-17 19:41:33 +0700

    done_ex2

--- js_bai_tap/Ex2.js
+++ js_bai_tap/Ex2.js
@@ -21,31 +21,25 @@ function getEle(id) {
 }
 
 
-getEle('tinhEx1').addEventListener('click', () => {
+getEle('tinhEx2').addEventListener('click', () => {
 
 
-    /* var soThuc_1 = 1;
-var soThuc_2 = 2;
-var soThuc_3 = 3;
-var soThuc_4 = 4;
-var soThuc_5 = 5;
-var tongCacSo = 0;
-var trungBinh = 0;
+    var soThuc_1 = parseFloat(getEle('soThuc1').value);
+    var soThuc_2 = parseFloat(getEle('soThuc2').value);
+    var soThuc_3 = parseFloat(getEle('soThuc3').value);
+    var soThuc_4 = parseFloat(getEle('soThuc4').value);
+    var soThuc_5 = parseFloat(getEle('soThuc5').value);
+    var tongCacSo = 0;
+    var giaTriTB = 0;
 
-tongCacSo =  soThuc_1 + soThuc_2 + soThuc_3 + soThuc_4 + soThuc_5;
-trungBinh = tongCacSo / 5;
+    tongCacSo = soThuc_1 + soThuc_2 + soThuc_3 + soThuc_4 + soThuc_5;
+    giaTriTB = tongCacSo / 5;
 
-console.log("Trung bình 5 số là: " + trungBinh); */
+    console.log("Trung bình 5 số là: " + giaTriTB);
 
-    var luongMotNgay = 100_000;
-    var soNgayLam = getEle('soNgayLam').value;
+    var kqEx2 = "Giá trị trung bình của 5 số: (" + soThuc_1 + " + " + soThuc_2 + " + " + soThuc_3 + " + " + soThuc_4 + " + " + soThuc_5 + ") / 5 = " + giaTriTB;
 
-    luongNhanVien = luongMotNgay * soNgayLam;
-
-    var currentFormat = new Intl.NumberFormat("vn-VN");
-    var moneyFormat = currentFormat.format(luongNhanVien);
-
-    console.log("Lương của nhân viên là: " + moneyFormat);
-
-    getEle('kqEx1').value = "Tổng tiền:      " + moneyFormat + " VND";
+    if (getEle('kqEx2').value === "") {
+        getEle('kqEx2').value = kqEx2;
+    }
 })
