15a658e5235409eb62628a51815473b2c8cf425d
tcdtist <chithanh.tist@gmail.com>
2021-01-24 18:14:19 +0700

    update validation.js

--- js_bai_tap/validation.js
+++ js_bai_tap/validation.js
@@ -11,7 +11,7 @@ function Validation() {
         }
         return true;
     };
-    this.kiemTraEx2 = (number1,number2,number3,number4,number5, min, mess, spanId) => {
+    this.kiemTraEx2 = (number1, number2, number3, number4, number5, min, mess, spanId) => {
         if (number1 <= min || number2 <= min || number3 <= min || number4 <= min || number5 <= min) {
             getEle(spanId).classList.add("thong-bao");
             getEle(spanId).innerHTML = mess;
@@ -19,4 +19,23 @@ function Validation() {
         }
         return true;
     };
+    this.kiemTraEx3 = (number, min, max, mess, spanId) => {
+        if (number <= min || number > max) {
+            getEle(spanId).classList.add("thong-bao");
+            getEle(spanId).innerHTML = mess;
+            return false;
+        }
+        return true;
+    };
+    // this.kiemTraSo = (number, mess, spanId) => {
+    //     var numberStr = "/^[0-9]{9}*$/";
+    //     console.log(numberStr)
+    //     if (number.match(numberStr)) {
+    //         console.log("đúng nè")
+    //         return true;
+    //     }
+    //     getEle(spanId).classList.add("thong-bao");
+    //     getEle(spanId).innerHTML = mess;
+    //     return false;
+    // }
 }
